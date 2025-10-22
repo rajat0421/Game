367b1134bd05065fe9a5cedc91cd586947761a1e
rajat0421 <rajattalekar5143@gmail.com>
2025-10-23 01:24:59 +0530

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,12 @@ const cookieParser = require('cookie-parser');
 const userRouter = require('./routes/user.router');
 const roomRouter = require('./routes/room.router');
 const guessRouter = require('./routes/guess.router');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
+
 
 app.use('/user',userRouter);
 app.use('/room',roomRouter);
