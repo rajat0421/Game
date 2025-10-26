eaaab2ce66960ec8f0e20cde963a52bb88520fb0
rajat0421 <rajattalekar5143@gmail.com>
2025-10-26 16:38:21 +0530

    updated user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username : { type: String, required: true, unique: true },
     // password : { type: String, required: true },
+    isLoggedIn: { type: Boolean,  default: false},
+    lastActive: { type: Date, default: Date.now },
     totalscore : { type: Number, default: 0 },
     joinedrooms : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Room' }]
 },
